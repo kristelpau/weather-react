c19ed5663c0cd71279f22713ef8dca73cb21c097
Kristel Pau <77642513+kristelpau@users.noreply.github.com>
2021-05-16 14:10:48 +0300

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <small>
+      <a href="https://github.com/kristelpau/weather-react" target="_blank">Open-source code</a> by Kristel Pau
+    </small>
     </div>
+    
   );
 }
 
